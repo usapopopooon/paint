17d45dbfefeb1a0766ffb527d276faac7cdc465e
usapo <usapopopooon@gmail.com>
2025-12-31 00:54:43 +0900

    style: スライダーのリング色をトラック未選択部分と統一

--- src/components/ui/slider.tsx
+++ src/components/ui/slider.tsx
@@ -50,7 +50,7 @@ function Slider({
         <SliderPrimitive.Thumb
           data-slot="slider-thumb"
           key={index}
-          className="border-control ring-control block size-4 shrink-0 rounded-full border bg-white shadow-sm transition-[color,box-shadow] hover:ring-4 focus-visible:ring-4 focus-visible:outline-hidden disabled:pointer-events-none disabled:opacity-50"
+          className="border-control ring-control/30 block size-4 shrink-0 rounded-full border bg-white shadow-sm transition-[color,box-shadow] hover:ring-4 focus-visible:ring-4 focus-visible:outline-hidden disabled:pointer-events-none disabled:opacity-50"
         />
       ))}
     </SliderPrimitive.Root>
