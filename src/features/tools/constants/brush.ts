0c6a5cf8b4b7df3dd073fb4d27235028cd18f3c6
usapopopooon <usapopopooon@gmail.com>
2026-01-11 04:26:55 +0900

    chore: ブラシツールのデフォルトサイズを20に変更

--- src/features/tools/constants/brush.ts
+++ src/features/tools/constants/brush.ts
@@ -3,6 +3,6 @@ export const MIN_BRUSH_WIDTH = 1
 /** ブラシの最大幅（ピクセル） */
 export const MAX_BRUSH_WIDTH = 300
 /** ブラシのデフォルト幅（ピクセル） */
-export const DEFAULT_BRUSH_WIDTH = 5
+export const DEFAULT_BRUSH_WIDTH = 20
 /** ブラシのデフォルト色 */
 export const DEFAULT_BRUSH_COLOR = '#000000'
